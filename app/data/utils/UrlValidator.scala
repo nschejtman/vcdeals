1cf8435afa4f6b2a0f3315ad845e9482fe3619cc
Nicolas Schejtman <nschejtman93@gmail.com>
2015-08-25 15:03:34 -0300

    Added url validator

--- app/data/utils/UrlValidator.scala
+++ app/data/utils/UrlValidator.scala
@@ -3,5 +3,15 @@ package data.utils
 object UrlValidator {
   def isValid(url : String) = true
 
+  def validate(url : String) = {
+    var aux = url
+    val start: Int = aux.indexOf("://")
+    if ((start > -1 && aux.indexOf("/", start + 1) < 0) || aux.indexOf("/") < 0) aux += "/"
+    start match {
+      case -1 => aux
+      case _ => aux.substring(start + 3)
+    }
+  }
+
 
 }
