328b9e02cf129f5ecd45c266f7c79a1d88a4d01d
Nicolas Schejtman <nicolas@Nicolass-MacBook-Pro.local>
2015-08-24 22:07:38 -0300

    Scrapper trait definition

--- app/data/scrapper/Scrapper.scala
+++ app/data/scrapper/Scrapper.scala
@@ -1,5 +1,10 @@
 package data.scrapper
 
-trait Scrapper {
-
+trait Scrapper[T]{
+  /**
+   * Given a link, obtain a list of the
+   * @param link link to the source web page
+   * @return sequence of obtained elements from source
+   */
+  def getContent(link : String) : Seq[T]
 }
