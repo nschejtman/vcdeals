8a4a30d72386979807349357f7b83f398b8898ae
Nicolas Schejtman <nschejtman93@gmail.com>
2015-08-27 08:35:55 -0300

    Scrapper demo working

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,37 @@
 package controllers
 
-import play.api.mvc._
+import data.scrapper.deal.DoubleIterationDealScrapper
+import models.Deal
+import net.utils.UrlValidator
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.libs.json.Json
+import play.api.mvc.{Action, Controller}
+
+import scala.concurrent.ExecutionContext.Implicits._
+import scala.concurrent.{Future, _}
+
 
 class Application extends Controller {
 
   def index() = Action {
     Ok(views.html.index.render())
   }
+
+  def demoDoubleIt = Action {
+    Ok(views.html.doubleIteratorScrapperDemo.render())
+  }
+
+  def getDeals = Action { implicit request =>
+    val urlStr: String = Form(single("url" -> text)).bindFromRequest().get
+    UrlValidator.isValid(urlStr) match {
+      case true =>
+        val deals: Seq[Deal] = DoubleIterationDealScrapper.getContent(urlStr)
+        Ok(Json.toJson(deals))
+      case false => BadRequest
+    }
+  }
+
 
 
 }
--- app/models/Deal.scala
+++ app/models/Deal.scala
@@ -1,6 +1,12 @@
 package models
 
+import play.api.libs.json.Json
+
 case class Deal(name : String, url: String) {
 
 
+}
+
+object Deal {
+  implicit val dealFormat = Json.format[Deal]
 }
