59a4ffe02f65f312f6fa97879f10393db4a2eca4
Nicolas Schejtman <nschejtman93@gmail.com>
2015-09-08 17:00:09 -0300

    Refactored Lavca & Empea scrappers

--- app/data/scrapper/fund/EMPEAScrapper.scala
+++ app/data/scrapper/fund/EMPEAScrapper.scala
@@ -3,26 +3,29 @@ package data.scrapper.fund
 import javax.inject.Inject
 
 import dal.FundDAO
+import data.scrapper.filter.{FundAsocFilter, SocialFilter, UrlReductionFilter}
 import data.utils.NameValidator
+import net.Url
 import org.jsoup.Jsoup
-import org.jsoup.nodes.{Element, Document}
+import org.jsoup.nodes.{Document, Element}
 
 import scala.concurrent.ExecutionContext
 
 class EMPEAScrapper @Inject()(fundDAO: FundDAO)(implicit ec: ExecutionContext) {
 
-  //TODO refactor scrapper style
+  //noinspection SpellCheckingInspection
   def run() = {
     val empeaDoc: Document = Jsoup.connect("http://empea.org/members/list-of-members/").userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6").get()
-    val auxArray: Array[Element] = Array.empty
-    for (link: Element <- empeaDoc.select("a[href]").toArray(auxArray)) {
-      val url: String = link.attr("abs:href")
-      if (!url.contains("lavca")) {
-        val name: String = NameValidator.validateName(link.text())
-        fundDAO.create(name, url)
+    val tupledLinks: Array[(String, Url)] = empeaDoc.select("a[href]").toArray(Array.empty[Element]).map(e => (e.text(), Url(e.attr("abs:href"))))
+    val filteredLinks: Array[(String, Url)] = tupledLinks.filter(t => SocialFilter.filter(t._2)).filter(t => UrlReductionFilter.filter(t._2)).filter(t => FundAsocFilter.filter(t._2))
+    filteredLinks.foreach(t => {
+      val name = t._1 match {
+        case "" => t._2.host
+        case " " => t._2.host
+        case _ => NameValidator.validateName(t._1)
       }
+      fundDAO.create(name, t._2.toString)
     }
+    )
   }
-
-
 }
--- app/data/scrapper/fund/LAVCAScrapper.scala
+++ app/data/scrapper/fund/LAVCAScrapper.scala
@@ -3,7 +3,9 @@ package data.scrapper.fund
 import javax.inject.Inject
 
 import dal.FundDAO
+import data.scrapper.filter.{FundAsocFilter, UrlReductionFilter, SocialFilter}
 import data.utils.NameValidator
+import net.Url
 import org.jsoup.Jsoup
 import org.jsoup.nodes.{Document, Element}
 
@@ -11,18 +13,20 @@ import scala.concurrent.ExecutionContext
 
 class LAVCAScrapper @Inject()(fundDAO: FundDAO)(implicit ec: ExecutionContext) {
 
-  //TODO refactor scrapper style
-  def run() =  {
+  //noinspection SpellCheckingInspection
+  def run() = {
     val lavcaDoc: Document = Jsoup.connect("http://lavca.org/membership/current-members-lavca/").get()
-    val auxArray: Array[Element] = Array.empty
-    for (link : Element <- lavcaDoc.select("a[href]").toArray(auxArray)){
-      val url: String = link.attr("abs:href")
-      if(!url.contains("lavca")) {
-        val name: String = NameValidator.validateName(link.text())
-        fundDAO.create(name, url)
+    val tupledLinks: Array[(String, Url)] = lavcaDoc.select("a[href]").toArray(Array.empty[Element]).map(e => (e.text(), Url(e.attr("abs:href"))))
+    val filteredLinks: Array[(String, Url)] = tupledLinks.filter(t => SocialFilter.filter(t._2)).filter(t => UrlReductionFilter.filter(t._2)).filter(t => FundAsocFilter.filter(t._2))
+    filteredLinks.foreach(t => {
+      val name = t._1 match {
+        case "" => t._2.host
+        case " " => t._2.host
+        case _ => NameValidator.validateName(t._1)
       }
+      fundDAO.create(name, t._2.toString)
     }
+    )
   }
-
 
 }
