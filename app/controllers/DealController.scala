9faacf3d6e34d3c2521dc0ce5ae6ead718a14f40
Nicolas Schejtman <nschejtman93@gmail.com>
2015-09-10 00:15:31 -0300

    Added Url extraction to deals hub. List submition to fix

--- app/controllers/DealController.scala
+++ app/controllers/DealController.scala
@@ -4,7 +4,9 @@ import javax.inject.Inject
 
 import com.google.inject.Singleton
 import dal.DealDAO
+import data.scrapper.deal.DoubleIterationDealScrapper
 import models.Deal
+import net.utils.UrlValidator
 import play.api.data.Form
 import play.api.data.Forms._
 import play.api.libs.json.Json
@@ -23,6 +25,20 @@ class DealController @Inject()(dealDao: DealDAO)(implicit ec: ExecutionContext) 
     )(Deal.apply)(Deal.unapply)
   )
 
+  case class Deals(deals: List[Deal])
+
+  val dealsForm = Form[Deals](
+    mapping(
+      "deals" -> list(
+        mapping(
+          "id" -> longNumber(),
+          "name" -> text(),
+          "url" -> text(),
+          "verified" -> boolean
+        )(Deal.apply)(Deal.unapply)
+      )
+    )(Deals.apply)(Deals.unapply)
+  )
 
   def getDealHub = Action {
     Ok(views.html.deal.hub.render())
@@ -43,6 +59,28 @@ class DealController @Inject()(dealDao: DealDAO)(implicit ec: ExecutionContext) 
         dealDao.create(deal.name, deal.url, deal.verified).map { _ =>
           Redirect(routes.DealController.getDealHub)
         }
+      }
+    )
+  }
+
+  def postExtractUrl = Action { implicit request =>
+    val urlStr: String = Form(single("url" -> text)).bindFromRequest().get
+    UrlValidator.isValid(urlStr) match {
+      case true =>
+        val deals: Seq[Deal] = DoubleIterationDealScrapper.getContent(urlStr)
+        Ok(Json.toJson(deals))
+      case false => BadRequest
+    }
+  }
+
+  def postList = Action { implicit request =>
+    dealsForm.bindFromRequest().fold(
+      errorForm => {
+        Redirect(routes.Application.index)
+      },
+      data => {
+        data.deals.foreach(deal => dealDao.create(deal.name, deal.url, deal.verified))
+        Ok(views.html.deal.hub.render())
       }
     )
   }
